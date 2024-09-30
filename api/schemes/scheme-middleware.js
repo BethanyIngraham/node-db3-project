87c1ea33bfcb9cd7b7349b6c824df150d7ed9064
Bethany Ingraham <BethFIngraham@gmail.com>
2024-09-30 00:30:09 -0400

    Check Scheme Id and validate scheme middleware working

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config');
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +8,26 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const {scheme_id} = req.params;
 
+    const schemeIdExists = await db('schemes')
+      .where('scheme_id', scheme_id)
+      .first();
+
+    if(schemeIdExists) {
+      next();
+    } else {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${scheme_id} not found`
+      });
+    }
+
+  } catch(err) {
+    next(err);
+  }
 }
 
 /*
@@ -19,7 +39,20 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
+  const {scheme_name} = req.body;
 
+  if(
+    scheme_name === undefined || 
+    typeof scheme_name !== 'string' || 
+    scheme_name.trim() === ''
+  ) {
+    next({
+      status: 400,
+      message: 'invalid scheme_name'
+    });
+  } else {
+    next();
+  }
 }
 
 /*
